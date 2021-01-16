186ff86791e086dc09533048c076b8014698fd54
Bowen Wu <51037053+Software-Cat@users.noreply.github.com>
2021-01-16 21:33:59 +1100

    Add docstring for Random Player

--- src/main/java/io/github/softwarecat/PlayerRandom.java
+++ src/main/java/io/github/softwarecat/PlayerRandom.java
@@ -28,14 +28,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * PlayerRandom is a Player who places bets in Roulette. This player makes random bets around the layout.
+ */
 public class PlayerRandom extends Player {
 
+    /**
+     * A Random Number Generator which will return the next random number.
+     * When writing unit tests, we will want to patch this with a mock object to return a known sequence of bets.
+     */
     protected final Random RNG;
 
+    /**
+     * All possible outcomes.
+     */
     protected final List<Outcome> ALL_OUTCOMES;
 
+    /**
+     * The Base bet.
+     */
     protected int baseBet = Game.TABLE_MINIMUM;
 
+    /**
+     * This uses the super() construct to invoke the superclass constructor using the Table.
+     * <p>
+     * It will also use the wheel associated with the table to get the set of bins. The set of bins is then used to create the
+     * pool of outcomes for creating bets.
+     *
+     * @param table the Table which will accept the bests
+     * @param rng   the random number generator
+     */
     public PlayerRandom(Table table, Random rng) {
         super(table);
         RNG = rng;
@@ -43,10 +65,14 @@ public class PlayerRandom extends Player {
     }
 
     /**
-     * Constructs the Player with a specific Table for placing Bets.
-     * Since the table has access to the Wheel, we can use this wheel to extract Outcome objects.
+     * This uses the super() construct to invoke the superclass constructor using the Table.
+     * <p>
+     * This will create a random.Random random number generator.
+     * <p>
+     * It will also use the wheel associated with the table to get the set of bins. The set of bins is then used to create the
+     * pool of outcomes for creating bets.
      *
-     * @param table the table to use
+     * @param table the Table which will accept the bests
      */
     public PlayerRandom(Table table) {
         this(table, new Random());
@@ -57,6 +83,11 @@ public class PlayerRandom extends Player {
         return (baseBet <= stake) && (roundsToGo > 0);
     }
 
+    /**
+     * Updates the Table with a randomly placed bet.
+     *
+     * @throws InvalidBetException if the Player attempts to place a bet which exceeds the table’s limit
+     */
     @Override
     public void placeBets() throws InvalidBetException {
         Outcome outcomeToBet = ALL_OUTCOMES.get(RNG.nextInt(ALL_OUTCOMES.size()));
